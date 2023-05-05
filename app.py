a27c51d0bcfa165db1c89c368c29e8e93f0ce755
ghassan-bounni <ghassan.bounni18@gmail.com>
2023-05-05 09:08:09 +0300

    fix output_dir not found bug

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
-import shutil
-
 import streamlit as st
 import os
 from PIL import Image
 from rembg import remove
 
 
-class Overlay_Product_App:
-    
+class OverlayProductApp:
+
     def construct_app(self):
         st.title("Overlay Product")
         uploaded_product = st.file_uploader("Upload Your Product")
@@ -19,7 +17,7 @@ class Overlay_Product_App:
         if uploaded_bg:
             try:
                 if not os.path.exists(bg_path):
-                    os.makedirs(bg_path)
+                    os.mkdir(bg_path)
                 else:
                     for img in os.listdir(bg_path):
                         os.remove(os.path.join(bg_path, img))
@@ -28,9 +26,12 @@ class Overlay_Product_App:
                     with open(bg_file_path, "wb") as f:
                         f.write(uploaded_bg[i].getbuffer())
 
-                Output_dir = 'Output_dir'
-                for img in os.listdir(Output_dir):
-                    os.remove(os.path.join(Output_dir, img))
+                output_dir = 'output_dir'
+                if not os.path.exists(output_dir):
+                    os.mkdir(output_dir)
+                else:
+                    for img in os.listdir(output_dir):
+                        os.remove(os.path.join(output_dir, img))
 
             except OSError as e:
                 st.write("Error occurred while saving background image: ", e)
@@ -38,27 +39,31 @@ class Overlay_Product_App:
             st.write("Result:")
             self.overlay_product(uploaded_product.name, bg_path)
 
-            for output_image_path in os.listdir(Output_dir):
-                st.image(os.path.join(Output_dir, output_image_path))
+            for output_image_path in os.listdir(output_dir):
+                st.image(os.path.join(output_dir, output_image_path))
 
-    def overlay_product(self, Product_Path, Background_Path):
-        Output_dir = 'Output_dir'
+    @staticmethod
+    def overlay_product(product_path, background_path):
+        output_dir = 'output_dir'
 
-        input_img = Image.open(Product_Path)
+        input_img = Image.open(product_path)
         cleaned = remove(input_img)
 
-        for i, bg in enumerate(os.listdir(Background_Path)):
-            # print(os.path.join(Background_Path, bg))
-            background = Image.open(os.path.join(Background_Path, bg))
+        for i, bg in enumerate(os.listdir(background_path)):
+            background = Image.open(os.path.join(background_path, bg))
             bg_w, bg_h = background.size
+
             resized = cleaned.resize((int(min(bg_w, bg_h) * 0.75), int(min(bg_w, bg_h) * 0.75)))
             img_w, img_h = resized.size
+
             offset = ((bg_w - img_w) // 2, (bg_h - img_h) // 2)
+
             background.paste(resized, offset, resized)
-            output_path = os.path.join(Output_dir, f"{i}.jpg")
+
+            output_path = os.path.join(output_dir, f"{i}.jpg")
             background.save(output_path)
 
 
 # Call the app
-app = Overlay_Product_App()
+app = OverlayProductApp()
 app.construct_app()
